8a1bec3cdbb1a3407e9dbf84aa64646c5ca3e85c
Sandhy <tiwarisandhyprakash@gmail.com>
2023-02-15 17:14:37 +0530

    Second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,4 +5,15 @@ public class Main {
     B n=new B();
     n.meth();
   }
+  public static class A{
+         public String meth(){
+           return "Invoking method from class A";
+         }
+  }
+  public static class B extends A{
+    @Override
+    public String meth() {
+      return "Method is overridden in Extendend class B";
+    }
+  }
 }
